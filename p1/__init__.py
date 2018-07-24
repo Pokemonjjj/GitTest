6ed3117a9eb3a5c63361bc3f299bdd6effeb860d
PokemonJ <pokemonjjj@outlook.com>
2018-07-24 22:27:42 +0800

    Initial commit

---
+++ p1/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time       : 2018/7/24 22:20
+@Author     : PokemonJ
+@File       : __init__.py.py
+@Software   : PyCharm
+@Description: ......
+"""
+
+if __name__ == "__main__":
+    print "test!!"
+    pass
+
